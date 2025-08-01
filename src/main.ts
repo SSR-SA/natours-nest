5b64ca22e5011b33d4423ba02d546d2998a5085a
SAI <108192790+SSR-SA@users.noreply.github.com>
2025-08-02 01:09:28 +0200

    Server initial setup along with prisma init and env setup included with supabase db link and password

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config'; // For accessing env variables
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = app.get(ConfigService);
+  const port = config.get<number>('PORT') ?? 9009;
+
+  await app.listen(port);
+  console.log(`App running on port ${port}...🚀`);
 }
 bootstrap();
